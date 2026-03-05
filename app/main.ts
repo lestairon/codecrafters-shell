7c3b69134de0fc723b83c87ddf0d731a989b5d44
Fernando <fernandonietop9@gmail.com>
2026-03-04 21:51:26 -0500

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -8,8 +8,14 @@ const rl = createInterface({
 
 rl.prompt();
 
+const commands = [
+  { name: "exit", description: "Exit the shell" },
+  { name: "echo", description: "Echo the arguments" },
+  { name: "type", description: "Type the arguments" },
+];
+
 rl.on("line", (line: string) => {
-  const [ command, ...args ] = line.trim().split(" ");
+  const { command, args } = parseCommand(line);
   // const args = line.trim().split(" ").slice(1);
 
   switch (command) {
@@ -20,9 +26,34 @@ rl.on("line", (line: string) => {
       console.log(args.join(" "));
       rl.prompt();
       break;
+    case "type":
+      const arg = args.join(" ");
+      if (commands.some((c) => c.name === arg)) console.log(`${arg} is a shell builtin`);
+      else console.log(`${arg}: not found`);
+      rl.prompt();
+      break;
     default:
       console.error(`${line}: command not found`);
       rl.prompt();
       break;
   }
 });
+
+function parseCommand(line: string) {
+  const [ command, ...args ] = line.trim().split(" ");
+  return { command: command.toLowerCase(), args };
+}
+
+// function runCommand(command: string, args: string[]) {
+
+//   const command = commands.find((c) => c.name === command);
+
+//   switch (command) {
+//     case "exit":
+//       rl.close();
+//       break;
+//     case "echo":
+//       console.log(args.join(" "));
+//       break;
+//   }
+// }
