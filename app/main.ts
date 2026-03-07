7cea0f4e2d84ae0ef0369d334b602517e1b2d4be
Fernando <fernandonietop9@gmail.com>
2026-03-06 20:42:31 -0500

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,7 @@
 import { createInterface } from "readline";
 import fs from "node:fs";
 import path from "node:path";
+import { execFileSync } from "node:child_process";
 
 const rl = createInterface({
   input: process.stdin,
@@ -25,14 +26,45 @@ const commands: Command[] = [
 rl.on("line", (line: string) => {
   const { command, args } = parseCommand(line);
   const commandObj = commands.find((c) => c.name === command);
+  const paths = process.env.PATH?.split(path.delimiter) ?? [];
 
-  if (commandObj) commandObj.run(args);
-  else console.error(`${line}: command not found`);
+  if (commandObj) {
+    commandObj.run(args);
+    rl.prompt();
 
+    return;
+  } else {
+    const cwd = process.cwd();
+    const filePath = path.join(cwd, command);
+    
+    // console.log(fs.existsSync(filePath), filePath);
+    if (fs.existsSync(filePath) && checkAccess(filePath)) {
+      // console.log({ filePath, args })
+      console.log(execFileSync(filePath, args));
+
+      rl.prompt();
+      return;
+    } else {
+      for (const dir of paths) {
+        const fullPath = path.join(dir, command);
+
+        if (fs.existsSync(fullPath) && checkAccess(fullPath)) {
+          console.log(execFileSync(fullPath, args, {
+            encoding: 'utf8'
+          }));
+
+          rl.prompt();
+          return;
+        }
+      }
+    }
+  }
+
+  console.error(`${line}: command not found`);
   rl.prompt();
 });
 
-function parseCommand(line: string) {
+function parseCommand(line: string): { command: string; args: string[] } {
   const [ command, ...args ] = line.trim().split(" ");
   return { command: command.toLowerCase(), args };
 }
@@ -51,9 +83,9 @@ function runType(args: string[]) {
     const filePath = path.join(pathObj, arg);
     if (fs.existsSync(filePath)) {
       try {
-      fs.accessSync(filePath, fs.constants.X_OK);
-      console.log(`${arg} is ${filePath}`);
-      return;
+        fs.accessSync(filePath, fs.constants.X_OK);
+        console.log(`${arg} is ${filePath}`);
+        return;
       } catch {}
     }
   }
@@ -64,4 +96,13 @@ function runType(args: string[]) {
 function runExit() {
   rl.close();
   process.exit(0);
+}
+
+function checkAccess(file: string): boolean {
+  try{
+    fs.accessSync(file, fs.constants.X_OK)
+    return true;
+  } catch {
+    return false;
+  }
 }
