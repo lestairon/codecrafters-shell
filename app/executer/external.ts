6bbec2fe31963903f04d4f9d3b4794c71c5239f8
Fernando <fernandonietop9@gmail.com>
2026-03-07 23:32:45 -0500

    spawn process by name instead of by full dir

--- app/executer/external.ts
+++ app/executer/external.ts
@@ -1,4 +1,5 @@
 import { spawnSync } from "node:child_process";
+import path from "node:path";
 import type { Command, CommandIO } from "../types";
 
 export function createExternalCommand(name: string, fullPath: string): Command {
@@ -6,7 +7,10 @@ export function createExternalCommand(name: string, fullPath: string): Command {
 		name,
 		description: `External command: ${name}`,
 		run: (args: string[], io: CommandIO) => {
-			const result = spawnSync(fullPath, args, { stdio: "pipe" });
+			const result = spawnSync(name, args, {
+				stdio: "pipe",
+				env: { ...process.env, PATH: `${path.dirname(fullPath)}:${process.env.PATH}` },
+			});
 			if (result.stdout) io.stdout.write(result.stdout.toString());
 			if (result.stderr) io.stderr.write(result.stderr.toString());
 		},
